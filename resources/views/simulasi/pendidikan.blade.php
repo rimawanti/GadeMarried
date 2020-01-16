b08cc32922801b2a7887dac71d7c3473c656a054
NeeraPuri <neerapurin@gmail.com>
2020-01-16 19:42:04 +0700

    form pendidikan 2

--- resources/views/simulasi/pendidikan.blade.php
+++ resources/views/simulasi/pendidikan.blade.php
@@ -17,7 +17,7 @@
             <div class="card">
               <div class="card-header border-0">
                 <div class="d-flex justify-content-between">
-                  <h1 class="card-title">LAKUKAN SIMULASI PROGRAM <b>RUMAH</b></h1>
+                  <h1 class="card-title">LAKUKAN SIMULASI PROGRAM <b>PENDIDIKAN</b></h1>
                   <!-- <a href="javascript:void(0);">Selengkapnya</a> -->
                 </div>
               </div>
@@ -36,11 +36,11 @@
                   <div class="form-group row row">
                     <label for="InputName" class="col-sm-2 col-form-label">Nama </label>
                     <div class="col-sm-10">
-                    	<input type="text" class="form-control" id="InputName" placeholder="Enter Nama Pasangan">
+                    	<input type="text" class="form-control" id="InputName" placeholder="Masukkan Nama Anda">
                     </div>
                   </div>
                   <div class="form-group row row">
-                    <label for="InputJangka" class="col-sm-2 col-form-label" >Rencana xxxxxxxxxxxx (dalam tahun)</label>
+                    <label for="InputJangka" class="col-sm-2 col-form-label" >Usia Anak Saat Ini (dalam tahun)</label>
                     <div class="col-sm-10">
                    	<input type="number" class="form-control" id="InputJangka" name="InputJangka" placeholder="Dalam tahun" value=1>
                 	</div>
@@ -51,7 +51,11 @@
                     <input type="text" step="0.01" class="form-control" id="InputGaji" name="InputGaji" placeholder="Enter gaji" value=2000000>
                 	</div>
                   </div>
-                  
+                  <div class="form-group row">
+                    <label for="InputGaji" class="col-sm-2 col-form-label">Biaya Sekolah TK saat ini:</label>
+                    <div class="col-sm-10">
+                    <input type="text" step="0.01" class="form-control" id="InputGaji" name="InputGaji" placeholder="Enter gaji" value=2000000>
+                	</div>
                   <!-- <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="Check1">
                     <label class="form-check-label" for="Check1">Check me out</label>

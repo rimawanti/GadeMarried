e55b6f691a8592fda00da3187367752576fef6eb
rimawanti <rfhyunki@gmail.com>
2020-03-09 14:32:05 +0700

    Update index.blade.php
    bug UI

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,8 +8,8 @@
 
 @section('content')
   <div class="row">
-          <div class="col-md-1">
-          </div>
+         <!--  <div class="col-md-1">
+          </div> -->
          
           <div class="col-lg-10">
             <div class="info-box bg-warning">
